c88eb2c06dc04e96653eb1268cd6ca0fe582b248
Alik <moisesalik01@gmail.com>
2023-11-19 13:47:26 -0500

    Clases de acceso a datos

--- pantallas/FrmMovimientos.Designer.vb
+++ pantallas/FrmMovimientos.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FrmMovimientos
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,58 @@ Partial Class FrmMovimientos
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.cbClientes = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgvMovimientos = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvMovimientos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cbClientes
+        '
+        Me.cbClientes.FormattingEnabled = True
+        Me.cbClientes.Location = New System.Drawing.Point(24, 40)
+        Me.cbClientes.Name = "cbClientes"
+        Me.cbClientes.Size = New System.Drawing.Size(321, 24)
+        Me.cbClientes.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Cliente"
+        '
+        'dgvMovimientos
+        '
+        Me.dgvMovimientos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvMovimientos.Location = New System.Drawing.Point(24, 84)
+        Me.dgvMovimientos.Name = "dgvMovimientos"
+        Me.dgvMovimientos.RowHeadersWidth = 51
+        Me.dgvMovimientos.RowTemplate.Height = 24
+        Me.dgvMovimientos.Size = New System.Drawing.Size(752, 354)
+        Me.dgvMovimientos.TabIndex = 2
+        '
+        'FrmMovimientos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dgvMovimientos)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cbClientes)
+        Me.Name = "FrmMovimientos"
         Me.Text = "FrmMovimientos"
+        CType(Me.dgvMovimientos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents cbClientes As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents dgvMovimientos As DataGridView
 End Class
