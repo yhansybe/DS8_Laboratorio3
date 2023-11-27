c57edb1bf637ca18d2dcb86fb982e631e6370168
alyson <alyson.torres@utp.ac.pa>
2023-11-26 20:52:17 -0500

    Tiene errores en FrmPrincipal

---
+++ clases/AgregarCuenta.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Data.SqlClient
+Imports MySql.Data.MySqlClient
+
+Public Class AgregarCuenta
+    Public Property IdCliente As Integer
+
+    Public Sub New(clienteId As Integer)
+        IdCliente = clienteId
+    End Sub
+
+    Public Sub AgregarCuentaEnBaseDeDatos(tipoCuenta As String, saldoInicial As Double)
+        Conexion.obtenerConexion()
+        Try
+            Dim command As New MySqlCommand("proc_insert_cuenta", Conexion.conexion)
+            command.CommandType = CommandType.StoredProcedure
+
+            command.Parameters.AddWithValue("@p_tipo", tipoCuenta)
+            command.Parameters.AddWithValue("@p_saldo", saldoInicial)
+            command.Parameters.AddWithValue("@p_id_cliente", IdCliente)
+            command.Parameters.Add("@resul", MySqlDbType.Bit)
+            command.Parameters("@resul").Direction = ParameterDirection.Output
+
+            command.ExecuteNonQuery()
+
+            Dim resultado As Boolean = Convert.ToBoolean(command.Parameters("@resul").Value)
+
+            If resultado Then
+                MessageBox.Show("Cuenta agregada con éxito.")
+            Else
+                MessageBox.Show("Error al agregar cuenta.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            Conexion.CerrarConexion()
+        End Try
+    End Sub
+End Class
--- pantallas/FrmAgregarCuenta.vb
+++ pantallas/FrmAgregarCuenta.vb
@@ -1,3 +1,27 @@
 ﻿Public Class FrmAgregarCuenta
+    Private clienteId As Integer
 
+    Public Sub New(clienteId As Integer)
+        InitializeComponent()
+        Me.clienteId = clienteId
+    End Sub
+
+    Private Sub btnAgregarCuenta_Click(sender As Object, e As EventArgs) Handles btnAgregarCuenta.Click
+        Dim tipoCuenta As String = cbTipoCuenta.SelectedItem.ToString()
+        Dim saldoInicial As Double
+
+        If Double.TryParse(tbSaldoInicial.Text, saldoInicial) Then
+            Dim agregarCuenta As New AgregarCuenta(clienteId)
+            agregarCuenta.AgregarCuentaEnBaseDeDatos(tipoCuenta, saldoInicial)
+        Else
+            MessageBox.Show("Ingrese un saldo válido.")
+        End If
+    End Sub
+
+    Private Sub FrmAgregarCuenta_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Configuración inicial del formulario
+        cbTipoCuenta.Items.Add("Cuenta Corriente")
+        cbTipoCuenta.Items.Add("Cuenta de Ahorros")
+        cbTipoCuenta.SelectedIndex = 0
+    End Sub
 End Class
